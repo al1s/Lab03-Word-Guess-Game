515cedfcb7ad74f784f5720b9f36303640a6b343
alstof <alstof@gmail.com>
2018-10-11 11:57:22 -0700

    Add file creation/addition

--- ConsoleWordGuessGame/Program.cs
+++ ConsoleWordGuessGame/Program.cs
@@ -1,9 +1,30 @@
 ﻿using System;
+using System.IO;
 
 namespace ConsoleWordGuessGame
 {
-    class Program
+    public class Program
     {
+        public static int CreateAppendTo(string path, string content)
+        {
+            int retCode = 1;
+            StreamWriter sw = File.AppendText(path);
+            try
+            {
+                sw.WriteLine(content);
+                retCode = 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Can't write to file");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (sw != null) sw.Close();
+            }
+            return retCode;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
--- ConsoleWordGuessGameTests/UnitTest1.cs
+++ ConsoleWordGuessGameTests/UnitTest1.cs
@@ -1,14 +1,17 @@
 using System;
 using Xunit;
+using ConsoleWordGuessGame;
+using System.IO;
 
 namespace ConsoleWordGuessGameTests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CanCreateFile()
         {
-
+            Program.CreateAppendTo("data.txt", "");
+            Assert.True(File.Exists("data.txt"));
         }
     }
 }
