9e65fe9814f4cf776030e4bda1b013511774e593
alstof <alstof@gmail.com>
2018-10-11 10:56:53 -0700

    Add project files.

---
+++ ConsoleWordGuessGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleWordGuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
